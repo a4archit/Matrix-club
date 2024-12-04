02ea71e04319821b9e87d8737617f50146873501
Archit <archittyagi599@gmail.com>
2024-12-04 21:17:22 +0530

    Update single_matrix_solutions.py

--- single_matrix_solutions.py
+++ single_matrix_solutions.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import numpy as np
+import sys
 
 # --------------------- Streamlit webpage designing --------------- #
 
-st.header(":orange[Matrix mall]")
+st.header(":orange[Nano Matrix]")
 st.divider()
 
 EXAMPLE_MATRIX_STR = """1,0,2
-3,1,2
-6,2,1"""
+    3,1,2
+    6,2,1"""
 
 st.write(f"""#### Single matrix
 You can continue with single matrix, with *n*-dimensions.
@@ -20,6 +21,12 @@ Example:\n
     {EXAMPLE_MATRIX_STR}
 """)
 
+EXAMPLE_MATRIX = np.array([
+    [1, 0, 2],
+    [3, 1, 2],
+    [6, 2, 1]
+])
+
 # matrix input label
 matrix_input_label = st.text_area(
     "Input matrix",
@@ -27,30 +34,6 @@ matrix_input_label = st.text_area(
     key="matrix_input_label",
     placeholder="Enter your matrix here"
 )
-
-reserved_space_for_done_button = st.empty()
-reserved_space_for_clear_button = st.empty()
-reserved_space_for_use_button = st.empty()
-
-
-# displaying given matrix
-st.write("Given Matrix is")
-# reserving space
-reserved_space_for_given_matrix = st.empty()
-
-
-
-
-# ------------------------ Declaring useful variables ----------------- #
-global input_matrix
-
-EXAMPLE_MATRIX = np.array([
-    [1, 0, 2],
-    [3, 1, 2],
-    [6, 2, 1]
-])
-
-
 
 
 # -------------------- Declaring useful functions --------------- #
@@ -65,84 +48,74 @@ def clear_input_field() -> None:
 
 def put_example_matrix_in_input_label() -> None:
     """ put the EXAMPLE matrix in input label """
-    st.session_state.matrix_input_label = EXAMPLE_MATRIX_STR
+    st.session_state.matrix_input_label = "1,0,2\n3,1,2\n6,2,1"
 
-def done_button_code() -> None:
-    """ This code runs when user click on `Done` button """
-    # getting user input data from the input label
-    user_input_data = matrix_input_label
-
-    # extracting matrix from user input data
-    temp_list = []
-    for row in user_input_data.split('\n'):
-        temp_list.append([int(element) for element in row.split(',')])
-    input_matrix = np.array(temp_list)
-
-    # getting matrix analysis report
-    matrix_report: dict = matrix_analysis(input_matrix)
-
-    # ---------------- Displaying section -------------- #
-    # user provided matrix
-    dynamic_msg(reserved_space_for_given_matrix,input_matrix)
-    global given_matrix
-    given_matrix = input_matrix
-
-    print('done function is successfully executed')
+@st.dialog("Invalid matrix!")
+def empty_matrix_dialog():
+    st.write("Input matrix field not be empty.\nUse example matrix or enter manually.")
 
 
 
 
-
-
-
-
-# ------------------- Taking matrix as input from user --------- #
-
-
+# ----------------- Adding buttons ------------------------------- #
 
 # done button
-reserved_space_for_done_button.button(
+done_btn = st.button(
     "Done",
-    type='primary',
-    on_click=done_button_code
+    key='done_btn_key',
+    type='primary'
 )
 
 # clear button
-reserved_space_for_clear_button.button(
+clear_btn = st.button(
     "Clear",
     on_click=clear_input_field
 )
+
 # use example as input button
-reserved_space_for_use_button.button(
+use_example_btn = st.button(
     "Use Example",
     on_click=put_example_matrix_in_input_label
 )
 
+if st.session_state.done_btn_key == True:
+    if matrix_input_label is None :
+        empty_matrix_dialog()
+    else:
+        try:
 
+            # extracting matrix from user input data
+            temp_list = []
+            for row in matrix_input_label.split('\n'):
+                temp_list.append([int(element) for element in row.split(',')])
+            input_matrix = np.array(temp_list)
 
+            st.divider()
 
+            # ---------------- Displaying outputs ----------------- #
+            analysis_report = {
+                "rank": np.linalg.matrix_rank(input_matrix),
+                "max_value": input_matrix.max(),
+                "min_value": input_matrix.min(),
+                "average": input_matrix.mean(),
+                "order": input_matrix.shape,
+                "squarred_matrix": input_matrix**2,
+                "transpose": input_matrix.transpose()
+            }
 
-# ************************* This code will shift in another file in future **********************
-def matrix_analysis(matrix: np.ndarray) -> dict:
-    """
-    Matrix analysis :
-    ================
-    Returns a python dictionary containing information about matrix(given as parameter).
-    Information collected by performing a lot of operations on the given matrix
+            st.write(f"## :green[Basic Information] \
+            \nMatrix rank           : **:blue[{analysis_report.get('rank')}]** \
+            \nOrder of matrix     : **:blue[{analysis_report.get('order')}]** \
+            \nAverage value       : **:blue[{analysis_report.get('average')}]** \
+            \nMin value           : **:blue[{analysis_report.get('min_value')}]** \
+            \nMax value           : **:blue[{analysis_report.get('max_value')}]**")
 
-    Parameters:
-    matrix: a numpy matrix
+            st.write(f"## :green[Transpose of matrix: ]")
+            st.write(analysis_report.get('transpose'))
 
-    Returns:
-    dictionary: {'title': value}
-    """
+            st.write(f"## :green[Square of matrix: ]")
+            st.write(analysis_report.get('squarred_matrix'))
 
-    analysis_report = {
-        "rank": np.linalg.matrix_rank(matrix),
-        "max_value": matrix.max(),
-        "min_value": matrix.min(),
-        "average": matrix.mean(),
-        "order": matrix.shape
-    }
+        except Exception as e:
+            print(e)
 
-    return analysis_report
