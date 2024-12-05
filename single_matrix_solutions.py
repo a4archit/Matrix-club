26d52569f54ae8a54fb0a11d1a4b361efcbf8688
Archit <archittyagi599@gmail.com>
2024-12-05 16:47:17 +0530

    Update single_matrix_solutions.py

--- single_matrix_solutions.py
+++ single_matrix_solutions.py
@@ -1,24 +1,27 @@
 import streamlit as st
 import numpy as np
-import sys
 
 # --------------------- Streamlit webpage designing --------------- #
 
-st.header(":orange[Nano Matrix]")
-st.divider()
+st.sidebar.title("About the developer")
+st.sidebar.divider()
+st.sidebar.write("I am try to create this web application through the use of \
+**Streamlit** and **NumPy**. You can check my social media accounts: ")
+st.sidebar.write("[Kaggle](https://www.kaggle.com/architty108)")
+st.sidebar.write("[Github](https://www.github.com/a4archit)")
+st.sidebar.write("[LinkedIn](https://www.linkedin.com/in/archit-tyagi-191323296)")
 
-EXAMPLE_MATRIX_STR = """1,0,2
+st.header("Nano Matrix", divider=True)
+
+EXAMPLE_MATRIX_STR = """
+    1,0,2
     3,1,2
     6,2,1"""
 
 st.write(f"""#### Single matrix
 You can continue with single matrix, with *n*-dimensions.
-\n**Note:** Your matrix pattern:\n
 Elements separate by comma`,` and
 rows separate by clicking `enter`.
-
-Example:\n 
-    {EXAMPLE_MATRIX_STR}
 """)
 
 EXAMPLE_MATRIX = np.array([
@@ -27,12 +30,15 @@ EXAMPLE_MATRIX = np.array([
     [6, 2, 1]
 ])
 
+reserved_space_for_use_example_btn = st.empty()
+
 # matrix input label
 matrix_input_label = st.text_area(
     "Input matrix",
     help="Use example matrix (if you have any problem in input section)",
     key="matrix_input_label",
-    placeholder="Enter your matrix here"
+    placeholder="Enter your matrix here",
+    label_visibility="visible"
 )
 
 
@@ -54,7 +60,9 @@ def put_example_matrix_in_input_label() -> None:
 def empty_matrix_dialog():
     st.write("Input matrix field not be empty.\nUse example matrix or enter manually.")
 
-
+def round_matrix(matrix: np.ndarray, decimals = 1) -> np.ndarray:
+    """ returns the matrix after rounding off """
+    return np.round(matrix, decimals=decimals)
 
 
 # ----------------- Adding buttons ------------------------------- #
@@ -63,18 +71,21 @@ def empty_matrix_dialog():
 done_btn = st.button(
     "Done",
     key='done_btn_key',
-    type='primary'
+    type='primary',
+    use_container_width=True
 )
+
 
 # clear button
 clear_btn = st.button(
     "Clear",
-    on_click=clear_input_field
+    on_click=clear_input_field,
+    use_container_width=True
 )
 
 # use example as input button
-use_example_btn = st.button(
-    "Use Example",
+reserved_space_for_use_example_btn.button(
+    f"Use this Example\n\n{EXAMPLE_MATRIX_STR}",
     on_click=put_example_matrix_in_input_label
 )
 
@@ -94,34 +105,58 @@ if st.session_state.done_btn_key == True:
 
             # ---------------- Displaying outputs ----------------- #
             analysis_report = {
-                "rank": np.linalg.matrix_rank(input_matrix),
-                "max_value": input_matrix.max(),
-                "min_value": input_matrix.min(),
-                "average": input_matrix.mean(),
-                "order": input_matrix.shape,
-                "determinant": np.linalg.det(input_matrix),
-                "inverse": np.linalg.inv(input_matrix),
-                "norm": np.linalg.norm(input_matrix),
-                "trace": np.linalg.trace(input_matrix), # results in the form of matrix
-                "squarred_matrix": input_matrix @ input_matrix,
-                "transpose": input_matrix.transpose(),
-                "element_squarred_matrix": input_matrix ** 2
+                # basic information
+                "rank"          : np.linalg.matrix_rank(input_matrix),
+                "max_value"     : input_matrix.max(),
+                "min_value"     : input_matrix.min(),
+                "average"       : input_matrix.mean(),
+                "order"         : input_matrix.shape,
+                "determinant"   : np.linalg.det(input_matrix),
+                "norm"          : np.linalg.norm(input_matrix),
+                "trace"         : np.linalg.trace(input_matrix),
+                # results in the form of matrix
+                "squared_matrix"        : input_matrix @ input_matrix,
+                "transpose"             : input_matrix.transpose(),
+                "inverse"       : np.linalg.inv(input_matrix),
+                "element_squared_matrix": input_matrix ** 2
             }
 
-            st.write(f"## :green[Basic Information] \
-            \nMatrix rank           : **:blue[{analysis_report.get('rank')}]** \
-            \nOrder of matrix     : **:blue[{analysis_report.get('order')}]** \
-            \nAverage value       : **:blue[{analysis_report.get('average')}]** \
-            \nMin value           : **:blue[{analysis_report.get('min_value')}]** \
-            \nMax value           : **:blue[{analysis_report.get('max_value')}]**")
+            # creating tabs for displaying the output
+            basic_information_tab, matrix_outputs_tab = st.tabs(['Information','Matrix Outputs'])
 
-            st.write(f"## :green[Transpose of matrix: ]")
-            st.write(analysis_report.get('transpose'))
-            st.write(f"## :green[Square of matrix: ]")
-            st.write(analysis_report.get('squarred_matrix'))
-            st.write(f"## :green[Element wise Square ]")
-            st.write(analysis_report.get('element_squarred_matrix'))
+            with basic_information_tab:
+                st.write(
+                    f"## :green[Basic Information] \
+                    \nMax value           : **:blue[{analysis_report.get('max_value')}]** \
+                    \nMin value           : **:blue[{analysis_report.get('min_value')}]** \
+                    \nMatrix rank         : **:blue[{analysis_report.get('rank')}]** \
+                    \nAverage value       : **:blue[{analysis_report.get('average')}]** \
+                    \nOrder of matrix     : **:blue[{analysis_report.get('order')}]** \
+                    \nMagnitude of matrix : **:blue[{round(analysis_report.get('norm'),2)}]** \
+                ")
+
+            with matrix_outputs_tab:
+                col1, col2 = st.columns(2)
+
+                with col1:
+                    # transpose
+                    st.write(f"## Transpose of matrix: ")
+                    st.write(analysis_report.get('transpose'))
+
+                    # matrix square
+                    st.write(f"## Square of matrix: ")
+                    st.write(analysis_report.get('squared_matrix'))
+
+                with col2:
+                    # matrix element-wise square
+                    st.write(f"## Element wise Square: ")
+                    st.write(analysis_report.get('element_squared_matrix'))
+
+                    # matrix inverse
+                    st.write(f"## Inverse of matrix: ")
+                    st.write(round_matrix(analysis_report.get('inverse')))
+
+
 
         except Exception as e:
             print(e)
-
